15df6bf674e9a83831697239e61b0c7f1dd5aea8
mustapha0101 <37918394+mustapha0101@users.noreply.github.com>
2024-12-05 16:07:52 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -44,8 +44,8 @@ class _UiSettings(BaseSettings):
     title: str = "Contoso"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "CORA"
+    chat_description: str = "Assistant d'aide au greffe"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
